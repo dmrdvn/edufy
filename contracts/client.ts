c3be7c710741df62f9ab45796f44e8108b485b49
dmrdvn <demirdoven.hh@gmail.com>
2024-08-25 21:41:17 -0400

    Fixed Wallet Connect

--- contracts/client.ts
+++ contracts/client.ts
@@ -2,24 +2,17 @@ import { createPublicClient, createWalletClient, http, custom } from 'viem';
 import { sepolia } from 'viem/chains';
 import { EthereumProvider } from '@walletconnect/ethereum-provider';
 
-// Public client can be created without any conditions as it doesn't rely on window object
 export const publicClient = createPublicClient({
   chain: sepolia,
   transport: http(),
 });
 
-let walletClient: ReturnType<typeof createWalletClient> | null = null;
 
-if (typeof window !== 'undefined' && window.ethereum) {
-  walletClient = createWalletClient({
-    chain: sepolia,
-    transport: custom(window.ethereum!),
-  });
-} else {
-  console.warn('window.ethereum is not available. This code runs on the server-side or without Metamask.');
-}
+export const walletClient = createWalletClient({
+  chain: sepolia,
+  transport: custom(window.ethereum!),
+});
 
-export { walletClient };
 
 export async function getWalletConnectClient() {
   const provider = await EthereumProvider.init({
